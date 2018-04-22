ab21b82ed5c439c126b1c749245852bfda339ba6
Mantas Radzvilavičius <m.radzvilavicius@gmail.com>
2018-04-22 11:12:08 +0300

    fix issues suggested in the review

--- README.md
+++ README.md
@@ -18,3 +18,31 @@ For specific, detailed instructions, look at the project instructions in the [Ud
 This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
 
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+
+##Playing the game
+
+This is a memory game which is played by flipping cards and trying to find a match. To win all 16 cards must be matched. 
+In case there is an incorrect guess the cards shake and are turned back.
+There are some stats available in the game:
+1) Star rating - user starts with 3 stars and 1 is removed after move 15 and the other is removed after move 20. 
+2) Timer - it simply calculates the time in minutes and seconds.
+3) Number of moves - defines 2 card flip as a move. The less moves - the better.
+
+##Built With
+
+All code is written in HTML, CSS and JavaScript.
+https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css - library for icons
+https://fonts.googleapis.com/css?family=Coda - used to style icons.
+https://fonts.googleapis.com/css?family=Merriweather+Sans - used to style the congratulations menu.
+
+##Code logic:
+
+Most parts are explained in the comments, but here is a brief explanation:
+Deck is cleared and array of cards is shuffled with the the shuffle() function tanken from: http://stackoverflow.com/a/2450976
+After that clicks are added to cards. After clicks are added the information about current click and previous click are stored into variables click and previousCLick.
+After that the code checks what is a match and what is not a match and prevents some unwanted behaviours of the game (like to be able to click 3 cards in a row).
+Finally, then there is the logic for the end menu - but it is quite understandable.
+
+#Author
+Mantas Radzvilavicius 
--- js/app.js
+++ js/app.js
@@ -1,32 +1,34 @@
 let timer = 0;
 let card = document.getElementsByClassName('card');
-let array = Array.from(card);
-let moves
+let cardsArray = Array.from(card);
+let moves;
 moves = 0;
 document.getElementsByClassName('moves')[0].textContent = `${moves}`;
 let deck = document.querySelector('.deck');
 let stars = document.getElementById('stars');
-let firstStar = stars.childNodes[1];
 let matches = 0;
 let restartButton = document.getElementById('yes');
 let noRestartButton = document.getElementById('no');
-let starsHtml = stars.innerHTML;
-let congratulationsStarsHtml;
+
+let minutes;
+let seconds;
+let time = document.getElementById('time');
 
 restartButton.addEventListener('click', function() {
     startNew();
-})
+});
 
+//necessary fuctions for the game to be restarted
 function clearDeck() {
-    deck.innerHTML = ""
+    deck.innerHTML = "";
 }
 
 function showStuff() {
-    for (let i = 0; i < array.length; i++) {
-        let temp = array[i];
+    for (let i = 0; i < cardsArray.length; i++) {
+        let temp = cardsArray[i];
         deck.appendChild(temp);
     }
-};
+}
 
 let eachCard = document.querySelectorAll('.card');
 
@@ -34,46 +36,46 @@ eachCard.forEach(function coverCards(item) {
     item.classList.toggle('match', false);
     item.classList.toggle('open', false);
     item.classList.toggle('show', false);
-})
+});
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length,
+function shuffle(cardsArray) {
+    var currentIndex = cardsArray.length,
         temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        temporaryValue = cardsArray[currentIndex];
+        cardsArray[currentIndex] = cardsArray[randomIndex];
+        cardsArray[randomIndex] = temporaryValue;
     }
 
-    return array;
+    return cardsArray;
 
 
 }
 
-
+//on page refresh to initialize the game
 clearDeck();
 
-shuffle(array);
+shuffle(cardsArray);
 
 showStuff();
 
-let icon = document.querySelectorAll('.icon');
-let iconArray = Array.from(icon);
-let click
-let previousClick
 
-let isIncorrect
+let icon = document.querySelectorAll('.icon');
+let click;
+let previousClick;
+
+let isIncorrect;
 
 icon.forEach(function(i) {
-    let cardElement = i.parentNode
+    let cardElement = i.parentNode;
     cardElement.addEventListener("click", addClicks);
 
-
+    //main function to define when a card can be clicked and what happens after clicking
     function addClicks() {
         //if prevents 3rd card to be clicked
         if (!isIncorrect && this.classList.contains('match') === false) {
@@ -84,23 +86,23 @@ icon.forEach(function(i) {
             previousClick = click;
             click = this;
 
-            //below part checks if it is a match or no match
+            //below part checks if it is a match or no match with some prerequisites to be met
             if (previousClick && (click.classList[2] == previousClick.classList[2]) && click.id !== previousClick.id && (previousClick.classList.contains('match') === false)) {
-                moves++
+                moves++;
                 document.getElementsByClassName('moves')[0].textContent = `${moves}`;
                 if (moves > 19 && moves < 21) {
                     stars.innerHTML = `<li><i class="fa fa-star"></i></li>`;
-                    //stars.removeChild(stars.childNodes[3]);
                 } else if (moves > 14 && moves < 16) {
                     stars.innerHTML = `<li><i class="fa fa-star"></i></li>
     <li><i class="fa fa-star"></i></li>`;
-                    //stars.removeChild(stars.childNodes[5]);
+
                 }
                 if (click.childNodes[1].classList[1] == previousClick.childNodes[1].classList[1]) {
                     click.classList.add('match');
                     previousClick.classList.add('match');
                     document.getElementsByClassName('moves')[0].textContent = `${moves}`;
                     matches += 2;
+                    //winning the game
                     if (matches === 16) {
                         printStats();
                     }
@@ -117,7 +119,7 @@ icon.forEach(function(i) {
                         click.classList.remove('nomatch');
                         previousClick.classList.remove('nomatch');
                         isIncorrect = false;
-                    }, 1000)
+                    }, 1000);
 
                 }
             }
@@ -126,12 +128,15 @@ icon.forEach(function(i) {
 
 });
 
+//timer
 setInterval(function() {
-        timer++
-    },
-    1000);
+    timer++;
+    minutes = Math.floor(timer / 60);
+    seconds = timer - minutes * 60;
+    time.innerHTML = `${minutes} min and ${seconds} sec`;
+}, 1000);
 
-let stats = document.getElementsByClassName('stats')[0].childNodes[1]
+let stats = document.getElementsByClassName('stats')[0].childNodes[1];
 let statsBox = document.getElementsByClassName('congratulations')[0];
 
 let restart = document.getElementsByClassName('restart')[0];
@@ -139,40 +144,41 @@ restart.addEventListener('click', function() {
     startNew();
 });
 
+//logic of what happens when restart button is pressed
 function startNew() {
     eachCard.forEach(function(item) {
         item.classList.remove('match');
         item.classList.remove('open');
         item.classList.remove('show');
-    })
-    shuffle(array);
+    });
+    shuffle(cardsArray);
     showStuff();
     timer = 0;
     moves = 0;
     stars.innerHTML = `<li><i class="fa fa-star"></i></li>
   <li><i class="fa fa-star"></i></li>
-  <li><i class="fa fa-star"></i></li>`
+  <li><i class="fa fa-star"></i></li>`;
     statsBox.classList.add('hidden');
     document.getElementsByClassName('moves')[0].textContent = `${moves}`;
     matches = 0;
 }
 
-
+//congratulations menu content
 function printStats() {
     statsBox.classList.remove('hidden');
-    stats.textContent = `You have spent ${timer} seconds and made ${moves} moves!`;
-if (moves <= 15){
-    stats.innerHTML += '<br>' + starsHTML;
-  } else if (moves > 15 && moves < 20) {
-    stats.innerHTML += '<br>' + `<li><i class="fa fa-star"></i></li>
-    <li><i class="fa fa-star"></i></li>`
-  } else {
-    stats.innerHTML += '<br>' + `<li><i class="fa fa-star"></i></li>`
-  }
+    stats.textContent = `You have spent ${minutes} min and ${seconds} sec and made ${moves} moves!`;
+    if (moves < 15) {
+        stats.innerHTML += '<br>' + `<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>`;
+    } else if (moves >= 15 && moves < 20) {
+        stats.innerHTML += '<br>' + `<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>`;
+    } else {
+        stats.innerHTML += '<br>' + `<li><i class="fa fa-star"></i></li>`;
+    }
 }
-
-
 
 noRestartButton.addEventListener('click', function() {
     statsBox.classList.add('hidden');
-})
+});
